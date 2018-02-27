2b231d675049e03087006798a22bccde750c5e4e
P.G.Golovkin <jzi@inbox.ru>
2018-02-27 21:57:01 +0300

    work with class e154

--- e154.cpp
+++ e154.cpp
@@ -1,6 +1,28 @@
 #include "e154.h"
 
+//typedef DWORD WINAPI (*GetVer)();
+
+class Error{};
+
 E154::E154()
 {
+    hDll = new QLibrary(name);
+    if(!hDll->isLoaded()) {throw Error();}
+}
 
+LPVOID E154::CallCreateLInstance()
+{
+    if(hDll == NULL) return NULL;
+    return (LPVOID)hDll->resolve("CreateLInstance");
+}
+
+LPVOID E154::CallGetDllVersion()
+{
+    if(hDll == NULL) return NULL;
+    return (LPVOID)hDll->resolve("GetDllVersion");
+}
+
+HINSTANCE E154::GetDllHinstance()
+{
+    return (HINSTANCE)hDll;
 }
--- e154.h
+++ e154.h
@@ -1,11 +1,30 @@
 #ifndef E154_H
 #define E154_H
 
+#include "windows.h"
+#include <QLibrary>
+
+typedef int (WINAPI *pGetDllVersion)(void);
+typedef LPVOID (WINAPI *pCreateInstance)(char *);
 
 class E154
 {
 public:
     E154();
+    ~E154();
+
+    // получение адреса функции интерфейса модуля
+    LPVOID WINAPI CallCreateLInstance(void);
+    // получение адреса функции версии DLL
+    LPVOID WINAPI CallGetDllVersion(void);
+    // получение идентификатора библиотеки
+    HINSTANCE WINAPI GetDllHinstance(void);
+
+private:
+    // идентификатор модуля DLL
+    //HINSTANCE
+    const char *name = "LUSBAPI";
+    QLibrary *hDll;
 };
 
 #endif // E154_H
