09e432a1b31ab501d6e5a233e96f76acc50d264b
Golovkin P.G <jzi@inbox.ru>
2018-03-05 15:47:57 +0400

    add useE154:: initPorts(), GetTDStatus(), SetChannel(), GetInformation()

--- useE154.h
+++ useE154.h
@@ -10,6 +10,14 @@
 
 using std::string;
 
+enum position {
+	ON, OFF
+}
+
+enum channel {
+	CH1, CH2 , CH3, Ch4, PP, L
+}
+
 struct Errore_E154
 {
     string err_msg;
@@ -25,10 +33,38 @@ public:
     string OpenDevice();
     string GetUserMessages() const;
     string GetUsbSpeed();
+    string GetInformation() const;
+    void SetChannel(channel ch, position pos) {
+    	swith(ch){
+    		if(pos == chennel::ON){
+	    		case CH1: { TtlOut |= (1<<0); pModule->TTL_OUT(TtlOut); break; }
+	    		case CH2: { TtlOut |= (1<<1); pModule->TTL_OUT(TtlOut); break; }
+	    		case CH3: { TtlOut |= (1<<2); pModule->TTL_OUT(TtlOut); break; }
+	    		case CH4: { TtlOut |= (1<<3); pModule->TTL_OUT(TtlOut); break; }
+	    		case  PP: { TtlOut |= (1<<4); pModule->TTL_OUT(TtlOut); break; }
+	    		case   L: { TtlOut |= (1<<5); pModule->TTL_OUT(TtlOut); break; }
+	    			default throw Errore_E154("Неправильно выбран TTL канал");
+	    	}else{
+				case CH1: { TtlOut &= ~(1<<0); pModule->TTL_OUT(TtlOut); break; }
+	    		case CH2: { TtlOut &= ~(1<<1); pModule->TTL_OUT(TtlOut); break; }
+	    		case CH3: { TtlOut &= ~(1<<2); pModule->TTL_OUT(TtlOut); break; }
+	    		case CH4: { TtlOut &= ~(1<<3); pModule->TTL_OUT(TtlOut); break; }
+	    		case  PP: { TtlOut &= ~(1<<4); pModule->TTL_OUT(TtlOut); break; }
+	    		case   L: { TtlOut &= ~(1<<5); pModule->TTL_OUT(TtlOut); break; }
+	    			default throw Errore_E154("Неправильно выбран TTL канал");
+	    	}
+    	}
+
+    }
+    bool GetTDStatus() { pModule->TTL_IN(&TtlIN); if(TtlIN & (1<<0)) return TRUE else return FALSE }  
 
 protected:
 	void initAPIInstance();
 	void initModuleHandler();
+	void initPorts() {
+		if(pModule->ENABLE_TTL_OUT(1)) pModule->TTL_OUT(0); else throw Errore_E154("Ошибка включения линий TTL");
+		//if(pModule->ENABLE_TTL_IN(1)) pModule->TTL_IN(0); else throw Errore_E154("Ошибка включения TTL_IN");
+	}
 	void ReleaseAPIInstance();					//(char *ErrorString, bool AbortionFlag);
     TLoadDll *pLoadDll;							// указатель на класс динамической загрузки DLL
 	DWORD DllVersion;							// версия библиотеки
@@ -41,6 +77,8 @@ protected:
 	const double AdcRate;						// частота работы АЦП в кГц
 	const WORD InputRangeIndex;					// индекс входного диапазона напряжения
 
+	WORD TtlOut;	//Состояние выходных портов 
+	WORD TtlIN;		//Состояние входных портов
     std::list<string> user_msg;
     //string user_msg;							// последнее выведенное сообщение
     string log_msg;							// служебная информация
