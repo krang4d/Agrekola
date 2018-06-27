938d651ec4659c04c6fda7ab9fafdb7809e0e010
P.G.Golovkin <jzi@inbox.ru>
2018-06-27 10:43:31 +0300

    add RealTime plot

--- widget.cpp
+++ widget.cpp
@@ -1,13 +1,15 @@
 ﻿#include "widget.h"
 #include "ui_widget.h"
 #include <QScrollBar>
+#include <QMessageBox>
 
 Widget::Widget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Widget)
 {
     ui->setupUi(this);
-    ui->frame->addGraph();
+    customPlot = ui->frame;
+    setupRealtimeData();
 }
 
 void Widget::setText(QString str)
@@ -20,36 +22,62 @@ void Widget::setAgrekola(useE154 *agr)
     Agrecola = agr;
 }
 
-void Widget::setupQuadraticPlot(QCustomPlot *customPlot, QVector<double> data)
+void Widget::setupQuadraticPlot(QVector<double> data)
 {
-
-    // generate some data:
-//    if (data.isEmpty() == 0)
-//    {
-//        QVector<double> x(101), y(101); // initialize with entries 0..100
-//        for (int i=0; i<101; ++i)
-//            {
-//              x[i] = i/50.0 - 1; // x goes from -1 to 1
-//              y[i] = x[i]*x[i];  // let's plot a quadratic function
-//            }
-//        customPlot->graph(0)->setData(x, y);
-//    }
-//    else
-//    {
-        QVector<double> x;
-        for(int i = 0; i < data.count(); i++)
-        {
-            x.append(double(i));
-        }
-        customPlot->graph(0)->setData(x, data);
-//    }
+    if (data.isEmpty())
+    {
+        QMessageBox::warning(this, tr("My Application"),
+                             tr("Нет данных для вывода графика.\n"));
+    }
+    customPlot->addGraph();
+    QVector<double> x;
+    double average;
+    for(int i = 0; i < data.count(); i++)
+    {
+        x.append(double(i));
+        average += data[i];
+    }
+    average /= data.length();
+    ui->label_average->setText(QString("%1").arg(average));
+    customPlot->graph(0)->setData(x, data);
     // give the axes some labels:
     customPlot->xAxis->setLabel("x");
     customPlot->yAxis->setLabel("y");
     // set axes ranges, so we see all data:
-    customPlot->xAxis->setRange(-1, data.length());
-    customPlot->yAxis->setRange(-3, 3);
+    customPlot->xAxis->setRange(0, data.length());
+    customPlot->yAxis->setRange(-5, 5);
     customPlot->replot();
+}
+
+void Widget::setupRealtimeData()
+{
+    // include this section to fully disable antialiasing for higher performance:
+    /*
+    customPlot->setNotAntialiasedElements(QCP::aeAll);
+    QFont font;
+    font.setStyleStrategy(QFont::NoAntialias);
+    customPlot->xAxis->setTickLabelFont(font);
+    customPlot->yAxis->setTickLabelFont(font);
+    customPlot->legend->setFont(font);
+    */
+    customPlot->addGraph(); // blue line
+    customPlot->graph(0)->setPen(QPen(QColor(40, 110, 255)));
+    customPlot->addGraph(); // red line
+    customPlot->graph(1)->setPen(QPen(QColor(255, 110, 40)));
+
+    QSharedPointer<QCPAxisTickerTime> timeTicker(new QCPAxisTickerTime);
+    timeTicker->setTimeFormat("%h:%m:%s");
+    customPlot->xAxis->setTicker(timeTicker);
+    customPlot->axisRect()->setupFullAxesBox();
+    customPlot->yAxis->setRange(-6, 6);
+
+    // make left and bottom axes transfer their ranges to right and top axes:
+    connect(customPlot->xAxis, SIGNAL(rangeChanged(QCPRange)), customPlot->xAxis2, SLOT(setRange(QCPRange)));
+    connect(customPlot->yAxis, SIGNAL(rangeChanged(QCPRange)), customPlot->yAxis2, SLOT(setRange(QCPRange)));
+
+    // setup a timer that repeatedly calls MainWindow::realtimeDataSlot:
+    connect(&dataTimer, SIGNAL(timeout()), this, SLOT(realtimeDataSlot()));
+    dataTimer.start(0); // Interval 0 means to refresh as fast as possible
 }
 
 Widget::~Widget()
@@ -83,8 +111,46 @@ void Widget::on_AdcSynchro_clicked()
 {
     Agrecola->vec_data.clear();
     setText(QString(Agrecola->AdcSynchro().c_str()));
-    setupQuadraticPlot(ui->frame, Agrecola->vec_data);
+    setupQuadraticPlot(Agrecola->vec_data);
     QScrollBar *vb = ui->textEdit->verticalScrollBar();
     int max = vb->maximum();
     vb->setValue(max);
+}
+
+void Widget::realtimeDataSlot()
+{
+    static QTime time(QTime::currentTime());
+    // calculate two new data points:
+    double key = time.elapsed()/1000.0; // time elapsed since start of demo, in seconds
+    static double lastPointKey = 0;
+    if (key-lastPointKey > 0.002) // at most add point every 2 ms
+    {
+      // add data to lines:
+      double a = Agrecola->AdcSample(CH1);
+      double b = Agrecola->AdcSample(CH2);
+      customPlot->graph(0)->addData(key, a); //qSin(key)+qrand()/(double)RAND_MAX*1*qSin(key/0.3843));
+      customPlot->graph(1)->addData(key, b); //qCos(key)+qrand()/(double)RAND_MAX*0.5*qSin(key/0.4364));
+      // rescale value (vertical) axis to fit the current data:
+      //ui->customPlot->graph(0)->rescaleValueAxis();
+      //ui->customPlot->graph(1)->rescaleValueAxis(true);
+      lastPointKey = key;
+    }
+    // make key axis range scroll with the data (at a constant range size of 8):
+    customPlot->xAxis->setRange(key, 8, Qt::AlignRight);
+    customPlot->replot();
+
+    // calculate frames per second:
+    static double lastFpsKey;
+    static int frameCount;
+    ++frameCount;
+//    if (key-lastFpsKey > 2) // average fps over 2 seconds
+//    {
+//      ui->statusBar->showMessage(
+//            QString("%1 FPS, Total Data points: %2")
+//            .arg(frameCount/(key-lastFpsKey), 0, 'f', 0)
+//            .arg(ui->customPlot->graph(0)->data()->size()+ui->customPlot->graph(1)->data()->size())
+//            , 0);
+//      lastFpsKey = key;
+//      frameCount = 0;
+//    }
 }
--- widget.h
+++ widget.h
@@ -2,6 +2,7 @@
 #define WIDGET_H
 #include <QString>
 #include <QWidget>
+#include <QTimer>
 #include "useE154.h"
 #include "QCustomPlot/qcustomplot.h"
 
@@ -17,18 +18,22 @@ public:
     explicit Widget(QWidget *parent = 0);
     void setText(QString str);
     void setAgrekola(useE154 *agr);
-    void setupQuadraticPlot(QCustomPlot *customPlot, QVector<double> data = {0});
+    void setupQuadraticPlot(QVector<double> data = {0});
+    void setupRealtimeData();
     ~Widget();
 
 private slots:
     void on_AdcSample_clicked();
     void on_AdcKadr_clicked();
     void on_AdcSynchro_clicked();
+    void realtimeDataSlot();
 
 private:
     Ui::Widget *ui;
     QString text;
     useE154 *Agrecola;
+    QTimer dataTimer;
+    QCustomPlot *customPlot;
 };
 
 #endif // WIDGET_H
